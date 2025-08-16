ef2b205d183c76492461fb501bd9e26a9aec60e5
Anshu Mishra <anshumishra1258@gmail.com>
2025-08-16 12:56:13 +0530

    fix: url change

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 // Utility for sending form data to backend
 export async function submitAnalysis(form, type) {
-  const response = await fetch('http://localhost:5000/api/analyze', {
+  const response = await fetch('https://backend-vanikai.onrender.com/api/analyze', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ type, form })
